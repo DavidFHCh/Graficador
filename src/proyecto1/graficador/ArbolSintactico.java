d98e86186b349b294c9112d333ecd59260d4e9e5
DAVID HERNANDEZ <davfep_96@hotmail.com>
2015-08-27 21:43:56 -0500

    Second commit

---
+++ src/proyecto1/graficador/ArbolSintactico.java
@@ -0,0 +1,28 @@
+package proyecto1.graficador;
+
+import java.util.*;
+
+/**
+ * Clase que realiza el Arobl Sintactico.
+ */
+public class ArbolSintactico{
+
+	private String s; // La String recibida.
+
+	/**
+	* Crea un ArbolSintactico.
+	* @param String s - La String que se analizara.
+	*/
+	public ArbolSintactico(String s){
+		this.s = s;
+	}
+
+	/**
+	* Metodo que agrega los "tokens" 
+	* al arbol Sintactico.
+	* @param
+	* @return void.
+	*/
+	public void agrega(String s){
+	}
+}
