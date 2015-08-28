4b2b8d2003044cf24e842a5d0b9abdc81c500a72
DAVID HERNANDEZ <davfep_96@hotmail.com>
2015-08-27 21:32:17 -0500

    First commit

---
+++ src/proyecto1/graficador/Fichero.java
@@ -0,0 +1,29 @@
+package proyecto1.graficador;
+
+import java.util.*;
+
+/**
+ * Clase que analiza sintacticamente las strings recibidas.
+ */
+public class Fichero{
+
+	private String s; // La String recibida.
+
+	/**
+	* Crea un Fichero.
+	* @param String s - La String que se analizara.
+	*/
+	public Fichero(String s){
+		this.s = s;
+	}
+
+	/**
+	* Metodo que crea los "tokens" para despues pasarselos7
+	* al arbol Sintactico.
+	* @param 
+	* @return LinkedList - Una lista con las fichas. 
+	*/
+	public LinkedList<String> hazFichas(){
+		return null;
+	}
+}
---
+++ src/proyecto1/graficador/Graficador.java
@@ -0,0 +1,13 @@
+/*
+* @author David Hernández. 
+*/
+package proyecto1.graficador;
+
+import java.util.*;
+
+public class Graficador{
+
+	public static void main(String[] args){
+		System.out.print("prueba.");
+	}
+}
