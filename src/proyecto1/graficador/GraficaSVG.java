8e85e70e9b0b7544ccccce82650c97180329345d
DAVID HERNANDEZ <davfep_96@hotmail.com>
2015-09-15 20:31:06 -0500

    Creando la clase Modelo(Graficador.java)

--- src/proyecto1/graficador/GraficaSVG.java
+++ src/proyecto1/graficador/GraficaSVG.java
@@ -12,27 +12,30 @@ public class GraficaSVG{
 	private double y2;
 	private int ancho;
 	private int alto;
-	private int proporcion;
+	private double proporcionX;
+    private double proporcionY;
 	private final static String[] colores = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
 
-	public GraficaSVG(int ancho, int alto,int proporcion ,double x1, double x2,double y1 , double y2, LinkedList<LinkedList<Double>> valoresY){
+	public GraficaSVG(int ancho, int alto,double proporcionX ,double proporcionY,double x1, double x2,double y1 , double y2, LinkedList<LinkedList<Double>> valoresY){
 		this.x1 = x1;
 		this.x2 = x2;
 		this.y1 = y1;
 		this.y2 = y2;
 		this.valoresY = valoresY;
-		this.proporcion = proporcion;
+		this.proporcionX = proporcionX;
+        this.proporcionY = proporcionY;
 		this.ancho = ancho;
 		this.alto = alto;
 		salida = new LinkedList<String>();
+        pintaGrafica();
 	}
 
 	public LinkedList<String> getSalida(){
 		return salida;
 	}
 
-	private String linea(double x1, double y1,String color){
-		return "<line x1='" + x1 + "' y1='" + y1 + "' x2='" + x1 + "' y2='" + y1 + "' stroke='" + color + "' stroke-width='3' />";
+	private String linea(double x1, double y1, double x2, double y2, String color){
+		return "<line x1='" + x1 + "' y1='" + y1 + "' x2='" + x2 + "' y2='" + y2 + "' stroke='" + color + "' stroke-width='3' />";
     }
 
     private String eje(int x1,int y1,int x2,int y2){
@@ -40,7 +43,7 @@ public class GraficaSVG{
     }
 
     private String dimensiones(int alto, int ancho){
-    	return "<svg width='" + ancho + "' height='" + 400 + "'>";
+    	return "<svg width='" + ancho + "' height='" + alto + "'>";
     }
 
     private String cierraSvg(){
@@ -49,17 +52,16 @@ public class GraficaSVG{
 
     private int coordenadaX(double x){
     	x = x - x1;
-    	return (int) (x*proporcion);
+    	return (int) (x*proporcionX);
     }
 
     private int coordenadaY(double y){
     	y = y2 - y;
-    	return (int) (y*proporcion);
+    	return (int) (y*proporcionY);
     }
 
     private void pintaGrafica(){
     	salida.add(dimensiones(alto,ancho));
-        System.out.println("entro?");
     	if(x1 < 0 && x2 > 0){
     		salida.add(eje(coordenadaX(0),0,coordenadaX(0),alto));
     	}
@@ -71,9 +73,15 @@ public class GraficaSVG{
 			String color = "#";
 				for(int j = 0; j < 6;j++)
 					color += colores[(int)(Math.random()*colores.length)];
+                double anterior = 0;
     		for(Double val: l){
-    			salida.add(linea(valX,coordenadaY(val),color));
+                if(anterior == 0){
+                    anterior = val;
+                    continue;
+                }
+    			salida.add(linea(valX-1,coordenadaY(anterior),valX,coordenadaY(val),color));
     			valX++;
+                anterior = val;
     		}
     	}
     	salida.add(cierraSvg());
--- src/proyecto1/graficador/Graficador.java
+++ src/proyecto1/graficador/Graficador.java
@@ -7,35 +7,56 @@ import java.util.*;
 
 public class Graficador{
 
+	private double x1;
+	private double x2;
+	private double y1;
+	private double y2;
+	private double x0;
+	private double proporcionX;
+	private double proporcionY;
+	private int ancho;
+	private int alto;
+	private String funcion;
+
+	public Graficador(double x1,double y1,double x2, double y2, int ancho, int alto, String funcion){
+
+	}
+
 	public static void main(String[] args){
 		LinkedList<LinkedList<Double>> ldeRes = new LinkedList<LinkedList<Double>>();
-		double x1 = -10;
+		double x1 = 0;
 		double x2 = 10;
-		double y1 = -10;
+		double y1 = -5;
 		double y2 = 10;
 		double x0 = x1;
 		int ancho = 800;
 		int alto = 600;
 		int pixeles = 0;
-		int proporcion = 0;
+		double proporcionX = 0;
+		double proporcionY = 0;
 		LinkedList<Double> resultados = new LinkedList<Double>();
 		if(x2 < x1 || y2 < y1)
 			throw new ExcepcionIntervalosInvalidos();
-		proporcion = (int)(ancho/(x2-x1));
-		while(proporcion == 0){
-			proporcion = (int)(pixeles*(ancho/x2-x1));
+		proporcionY = alto/(y2-y1);
+		proporcionX = (ancho/(x2-x1));
+		while(proporcionX == 0){
+			proporcionX = (pixeles*(ancho/x2-x1));
+			pixeles++;
+		}
+		pixeles = 0;
+		while(proporcionY == 0){
+			proporcionY = (pixeles*(ancho/y2-y1));
 			pixeles++;
 		}
 		Evaluador eval = new Evaluador();
-		eval.hazFichas("x");
+		eval.hazFichas("x^3");
 		eval.setSalida(eval.analizar(eval.getFichas()));
 		while(x0 <= x2){
 			resultados.add(eval.evalua(eval.getSalida(),x0,true));
-			System.out.println("que pedo?");
-			x0 += 2/proporcion;
+			x0 += 1/proporcionX;
 		}
 		ldeRes.add(resultados);
- 		GraficaSVG svg = new GraficaSVG(800,600,proporcion,x1,x2,y1,y2,ldeRes);
+ 		GraficaSVG svg = new GraficaSVG(ancho,alto,proporcionX,proporcionY,x1,x2,y1,y2,ldeRes);
  		LinkedList<String> grafSvg = svg.getSalida();
  		while(grafSvg.peek() != null){
  			System.out.println(grafSvg.poll());
